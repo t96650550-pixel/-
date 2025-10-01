badc9ca4b384cd5041ce949c3b3cbc6c5cec6468
t96650550-pixel <t96650550@gmail.com>
2025-10-01 12:47:44 +0700

    Update api.js

--- Server/client/src/api.js
+++ Server/client/src/api.js
@@ -1,4 +1,11 @@
+// api.js
 import axios from 'axios';
-const API = process.env.REACT_APP_API || 'http://localhost:4000';
-const instance = axios.create({ baseURL: API });
+
+const API = process.env.REACT_APP_API || '';
+const instance = axios.create({
+  baseURL: API,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: false
+});
+
 export default instance;
