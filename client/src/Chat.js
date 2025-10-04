b60ec63b4666570448757c15793d930478c17658
t96650550-pixel <t96650550@gmail.com>
2025-10-04 02:07:55 +0000

    Update Chat.js

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 import axios from 'axios';
+import './Chat.css';
 
 const API = process.env.REACT_APP_API || 'http://localhost:3001';
 
