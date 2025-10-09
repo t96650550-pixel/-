cf9f36e2a5bd89a50ea25c4d3cdb4a74f2531f29
t96650550-pixel <t96650550@gmail.com>
2025-10-09 12:13:40 +0700

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import '../styles/globals.css'
+import { useEffect } from 'react'
+import { supabase } from '../lib/supabaseClient'
+
+function MyApp({ Component, pageProps }) {
+  // optional: handle auth state change to persist session
+  useEffect(() => {
+    const { data: d } = supabase.auth.onAuthStateChange((event, session) => {
+      // optional: save to localStorage or cookie
+    })
+    return () => d.unsubscribe()
+  }, [])
+
+  return <Component {...pageProps} />
+}
+
+export default MyApp
